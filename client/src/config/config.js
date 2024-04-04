d2b659b2020be72876b0cc23e139eb0a51968eea
Abdulrahman-Haji <77126546+ENG-CJ@users.noreply.github.com>
2024-04-04 22:04:03 +0300

    changes url

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,2 +1,2 @@
-// export const API_BASE_URL = "https://node-cloundinary-server-loader.onrender.com"
-export const API_BASE_URL = "http://localhost:3200"
+export const API_BASE_URL = "https://clound-image-server.vercel.app";
+// export const API_BASE_URL = "http://localhost:3200"
